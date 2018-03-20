1612c35f4c2fe384681bc8ce86e4d641ea2bc5e8
Bohao Cao <bcao@ezesoft.com>
2018-03-20 08:31:06 -0400

    wordbreak and lca

---
+++ src/Common/TreeNode.java
@@ -0,0 +1,14 @@
+package Common;
+
+public class TreeNode {
+    /**
+     * Definition for a binary tree node.
+     */
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x) {
+            val = x;
+        }
+}
---
+++ src/LeetCode/wordBreak/Solution.java
@@ -0,0 +1,43 @@
+package LeetCode.wordBreak;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words. You may assume the dictionary does not contain duplicate words.
+
+        For example, given
+        s = "leetcode",
+        dict = ["leet", "code"].
+
+        Return true because "leetcode" can be segmented as "leet code".
+
+        UPDATE (2017/1/4):
+        The wordDict parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.*/
+public class Solution {
+    //Solution1 bfs
+    //Solution2 dynamic programming
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return dpSolution(s, wordDict);
+
+    }
+
+    private boolean dpSolution(String s, List<String> wordDict){
+        boolean[]dp= new boolean[s.length()+1];
+        Arrays.fill(dp, false);
+        dp[0]=true;
+        for(int i=1;i<=s.length();i++){
+            boolean found = false;
+            for(int j=0;j<i;j++){
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                    found = true;
+                    break;
+                }
+            }
+            dp[i] = found;
+        }
+        return dp[s.length()];
+    }
+
+
+}
