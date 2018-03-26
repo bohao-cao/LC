f47f8b0639076282323e6193a36721a3faddc9e0
Bohao Cao <bcao@ezesoft.com>
2018-03-26 09:03:53 -0400

    Add solution intelliJ template and title formatter

---
+++ src/Common/titleFormatter.java
@@ -0,0 +1,15 @@
+package Common;
+
+import org.junit.jupiter.api.Test;
+
+class TitleFormatter {
+    @Test
+    void calPoints() {
+
+        String title= "Maximum Product Subarray";
+        title = title.replaceAll("\\s+","");
+        title = title.substring(0, 1).toLowerCase() + title.substring(1);
+        System.out.println("val = " + title);
+    }
+
+}
