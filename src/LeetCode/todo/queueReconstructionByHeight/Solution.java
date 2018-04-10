86cd35926b8008b7df552fcb7aae497b723176bf
Bohao Cao <bohao.cao@gmail.com>
2018-04-09 22:10:57 -0400

    start of the problem

---
+++ src/LeetCode/todo/queueReconstructionByHeight/Solution.java
@@ -0,0 +1,20 @@
+package LeetCode.todo.queueReconstructionByHeight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Input:
+//[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+//
+//Output:
+//[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+public class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        List<int[]> res =new ArrayList<>();
+        for(int i=0;i< people.length;i++){
+            if(people[i][1] == 0)
+        }
+
+
+    }
+}
